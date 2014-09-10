e93edbf4161c261ee627d382ccd10340906d1b0c
Mike Knueppel <mikeknueppel@hotmail.com>
2014-09-10 10:09:53 -0700

    Add Angular

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 1.2'
+gem 'bower-rails'
 
 group :doc do
   gem 'sdoc', require: false
---
+++ app/assets/javascripts/angular-app/main.js
@@ -0,0 +1,9 @@
+angular.module('nrs')
+  .factory('Address', function() {
+    return {
+      state: 'CA'
+    };
+  })
+  .controller('InvoicesCtrl', function($scope, Address) {
+    $scope.hospitalState = Address.state;
+  });
