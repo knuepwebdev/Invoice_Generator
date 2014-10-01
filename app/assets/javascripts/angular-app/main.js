c4c0a6dcc4cad345007cb62b425d0ca3b05f1e18
Mike Knueppel <mikeknueppel@hotmail.com>
2014-10-01 16:50:47 -0700

    $watch the select box

--- app/assets/javascripts/angular-app/main.js
+++ app/assets/javascripts/angular-app/main.js
@@ -7,4 +7,20 @@ angular.module('nrs')
   .controller('InvoicesCtrl', ['$scope', 'Address', function($scope, Address) {
     $scope.hospitalState = Address.state;
     $scope.options = ['No', 'Yes'];
+    $scope.selectedOption = 'No';
+  }])
+  .directive('toggleParts', [function() {
+    function link(scope, element, attrs) {
+      scope.$watch(attrs.toggleParts, function(value) {
+        if (value === 'No') {
+          console.log(value);
+        } else {
+          console.log(value);
+        }
+      });
+    }
+
+    return {
+      link: link
+    };
   }]);
