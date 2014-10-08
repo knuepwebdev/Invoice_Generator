00e81361d3a09dfab132b522023522464138d132
Mike Knueppel <mikeknueppel@hotmail.com>
2014-10-08 12:01:43 -0700

    Align labor, travel, and mileage data

--- app/assets/javascripts/nrs.js
+++ app/assets/javascripts/nrs.js
@@ -1,4 +1,19 @@
 $(function() {
   $('#invoice_creator_service_report_date').datepicker();
   $('#invoice_date').datepicker();
+
+  // align labor data
+  $('#subtotal-labor').offset({top: $('#labor').offset().top});
+  //align travel data
+  $('#select_travel').change(function() {
+    if ( $('#select_travel').val() === '1') {
+      $('#subtotal-travel').offset({top: $('#travel').offset().top});
+    }
+  });
+  //align mileage data
+  $('#select_mileage').change(function() {
+    if ( $('#select_mileage').val() === '1') {
+      $('#subtotal-mileage').offset({top: $('#mileage').offset().top});
+    }
+  });  
 });
