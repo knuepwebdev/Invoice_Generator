ddf9af9efa0fd986093317135f3edf972bc35122
Mike Knueppel <mikeknueppel@hotmail.com>
2014-10-14 18:21:09 -0700

    Refactor setProperty method

--- app/assets/javascripts/angular-app/services/invoice.js
+++ app/assets/javascripts/angular-app/services/invoice.js
@@ -1,20 +1,6 @@
-                   // ng-change="getData('name',
-                   //            invoice.datas.invoice.client.name,
-                   //            invoice.datas.invoice.client)" />
-
 angular.module('invoice')
   .factory('Invoice', function() {    
     function setProperty(key, value, path) {
-      console.log('setProperty');
-      console.log('path: ' + path);
-      console.log('key: ' + key);
-      console.log('value: ' + value);
-      // console.log(Object.getOwnPropertyNames(path));
-      
-      // key_invoice = Object.getOwnPropertyNames(data.datas)
-      // console.log(Object.getOwnPropertyNames(data.datas[key]));
-      // key_client = Object.getOwnPropertyNames(data.datas[key]);
-      // var prop = attr;
       path[key] = value;
       console.log('Invoice service');
       console.log(Invoice);
@@ -22,18 +8,7 @@ angular.module('invoice')
     };
 
     var Invoice = {
-      setProperty: setProperty,
-      datas: {
-        invoice: {
-          client: {
-            name: '', 
-            street: '',
-            city: '',
-            state: '',
-            zipcode: ''
-          }
-        }
-      }
+      setProperty: setProperty
     };
 
     console.log('Invoice factory returning Invoice');
