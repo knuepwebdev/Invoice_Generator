78cfe80a57df2064cc2c06edf1f2bd1b0375ab1d
Mike Knueppel <mikeknueppel@hotmail.com>
2014-10-15 22:16:16 -0700

    Extract invoice data format into a class

--- app/models/invoice_creator.rb
+++ app/models/invoice_creator.rb
@@ -2,36 +2,27 @@ class InvoiceCreator
   extend ActiveModel::Naming
   include ActiveModel::Conversion
   include ActiveModel::Validations
-  # attr_reader :invoice_number, :invoice_date, :client_name, :street, :city, :state, :zipcode, :hospital_department, :hospital_room, :service_report_number, :service_report_date, :machine_make, :machine_model, :labor, :labor_hourly_rate, :travel, :travel_hourly_rate, :mileage, :mileage_rate, :part
 
-  def initialize(hash)
+  def initialize(invoice)
     puts 'initialize'
-    puts hash.inspect
-    hash.each do |key, value|
-      instance_variable_set("@#{key}", value)
-      self.class.send(:define_method, key.to_sym) do
-        # return the value of the instance variable
-        instance_variable_get("@#{key}")
-      end
-
-    end
+    @invoice = invoice
+    puts @invoice.inspect
   end
 
   def create
     puts 'create_invoice'
     puts self.inspect
     puts 'end create_invoice'
+    invoice_format = InvoiceFormat.new(@invoice)
     hospital = Hospital.new(
-      name: @client['name'],
-      department: @client['department'],
-      room: @client['room']
-    )
+      name: invoice_format.hospital_name,
+      department: invoice_format.hospital_department,
+      room: invoice_format.hospital_room)
     hospital.build_contact(
-      street: @client['contact']['street'],
-      city: @client['contact']['city'],
-      state: @client['contact']['state']['abbreviation'],
-      zipcode: @client['contact']['zipcode']
-    )
+      street: invoice_format.hospital_street,
+      city: invoice_format.hospital_city,
+      state: invoice_format.hospital_state,
+      zipcode: invoice_format.hospital_zipcode)
     hospital.save
   end
 end
---
+++ app/models/invoice_format.rb
@@ -0,0 +1,26 @@
+class InvoiceFormat
+  def initialize(invoice_format)
+    @invoice_format = invoice_format
+  end
+  def hospital_name
+    @invoice_format['client']['name']
+  end
+  def hospital_department
+    @invoice_format['client']['department']
+  end
+  def hospital_room
+    @invoice_format['client']['room']
+  end
+  def hospital_street
+    @invoice_format['client']['contact']['street']
+  end
+  def hospital_city
+    @invoice_format['client']['contact']['city']
+  end
+  def hospital_state
+    @invoice_format['client']['contact']['state']['abbreviation']
+  end
+  def hospital_zipcode
+    @invoice_format['client']['contact']['zipcode']
+  end         
+end
