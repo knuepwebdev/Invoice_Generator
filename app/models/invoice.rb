89cb5d143d966365c4668d158187ba5b12388335
Mike Knueppel <mikeknueppel@hotmail.com>
2014-10-22 13:47:57 -0700

    Add backend model validations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,5 @@
 class Invoice < ActiveRecord::Base
   belongs_to :service_report  
+  validates_presence_of :number, :date
+  validates_length_of :number, is: 4
 end
--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,4 @@
 class Part < ActiveRecord::Base
   belongs_to :service_report
+  validates_presence_of :number, :price, :quantity, :name
 end
