20e99f3a4e980bb161255dde84ffb78c958a862a
Mike Knueppel <mikeknueppel@hotmail.com>
2014-10-27 11:26:04 -0700

    Add vertical-offset attribute

--- app/assets/javascripts/angular-app/directives/nrs-align-parts.js
+++ app/assets/javascripts/angular-app/directives/nrs-align-parts.js
@@ -2,15 +2,12 @@ angular.module('invoice')
   .directive('nrsAlignParts', function() {
     function link(scope, element, attrs) {
       function alignParts(index) {
-        console.log('alignParts');
-        console.log(element);
-        console.log($('#part-name-' + (index + 1)));
-          var top = $('#part-name-' + (index + 1)).offset().top;
-          var taxTop = $('#input-tax').offset().top;
-          $('#part-quantity-' + (index + 1)).offset({top: top});   
-          $('#part-number-' + (index + 1)).offset({top: top});   
-          $('#part' + (index + 1) + '-subtotal').offset({top: top});   
-          $('#total-tax').offset({top: taxTop});   
+        var top = $('#part-name-' + (index + 1)).offset().top;
+        var taxTop = $('#input-tax').offset().top;
+        $('#part-quantity-' + (index + 1)).offset({top: top});   
+        $('#part-number-' + (index + 1)).offset({top: top});   
+        $('#part' + (index + 1) + '-subtotal').offset({top: top});   
+        $('#total-tax').offset({top: taxTop});   
       }
 
       scope.$watch(attrs.nrsAlignParts, function(value) {
